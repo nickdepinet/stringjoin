469863d86962d278f177c027cba5445ba502541a
Nick Depinet <nick@nick-virtual-machine.(none)>
2013-07-24 13:08:00 -0400

    Adding tests

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 stringjoin
 ==========
 
-Speed Testing Python's string join's
+This is meant as a basic speed comparison between python's + and String.join() methods. It is based on Wayne Werner's Stack Overflow answer here: http://stackoverflow.com/a/3055558. I've attempted to correct for some inefficiencies caused by him doing a .append when testing join.
+
+To run:
+original:
+    python stringconcat.py
+my fix:
+    python stringfixed.py
+    
+Not meant to be anything super qualitative, just a useful little test of strings of different lengths.
---
+++ stringconcat.py
@@ -0,0 +1,37 @@
+import time
+
+def strcat(string):
+    newstr = ''
+    for char in string:
+        newstr += string
+    return newstr
+
+def listcat(string):
+    chars = []
+    for char in string:
+        chars.append(char)
+    return ''.join(chars)
+
+def test(fn, times, *args):
+    start = time.time()
+    for x in xrange(times):
+        fn(*args)
+    return time.time() - start
+
+def testall():
+    strings = ['a', 'long', 'longer', 'a bit longer', 
+               '''adjkrsn widn fskejwoskemwkoskdfisdfasdfjiz  oijewf sdkjjka dsf sdk siasjk dfwijs''',
+               '''this is a really long string that's so long
+               it had to be triple quoted  and contains lots of
+               superflous characters for kicks and gigles
+               @!#(*_#)(*$(*!#@&)(*E\xc4\x32\xff\x92\x23\xDF\xDFk^%#$!)%#^(*#''',
+              '''I needed another long string but this one won't have any new lines or crazy characters in it, I'm just going to type normal characters that I would usually write blah blah blah blah this is some more text hey cool what's crazy is that it looks that the str += is really close to the O(n^2) worst case performance, but it looks more like the other method increases in a perhaps linear scale? I don't know but I think this is enough text I hope.''']
+
+    print 'Testing join'
+    for string in strings:
+        print "String of len:", len(string), "took:", test(listcat, 1000000, string), "seconds"
+    print 'Testing + concat'
+    for string in strings:
+        print "String of len:", len(string), "took:", test(strcat, 1000000, string), "seconds"
+    print 'Testing Complete'
+testall()
